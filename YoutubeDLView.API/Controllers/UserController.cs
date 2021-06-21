ececa4a5787e193317bc557a1a55d4d09a7f53c8
AnOwlbear <AnOwlbear@protonmail.com>
2021-06-21 18:34:13 +0100

    Updated CreateSetupUser to fail if already completed

--- YoutubeDLView.API/Controllers/UserController.cs
+++ YoutubeDLView.API/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using YoutubeDLView.Application.Services;
@@ -32,13 +33,15 @@ namespace YoutubeDLView.API.Controllers
         /// <param name="username">The username of the user to create</param>
         /// <param name="password">The password of the user to create</param>
         /// <response code="200">Successfully created setup account</response>
-        /// <response code="400">Account with the same name already exists</response>
+        /// <response code="400">Setup already completed</response>
         /// <returns></returns>
         [HttpPost("Setup")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> CreateSetupUser(string username, string password)
         {
+            if (_userManager.Users.Any()) return BadRequest("Setup already completed");
+            
             Result result = await _userManager.CreateUser(username, password, UserRole.Administrator);
             return result.Success switch
             {
