663fc6fab989b1cb3f1587d7ab5b6aaf033d573d
AnOwlbear <AnOwlbear@protonmail.com>
2021-07-16 23:55:15 +0100

    Added WIP EntityChanges

---
+++ YoutubeDLView.Core/Common/EntityUpdate.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+
+namespace YoutubeDLView.Core.Common
+{
+    public class EntityUpdate<T>
+    {
+        private readonly object _entityChanges;
+
+        public EntityUpdate(object entityChanges)
+        {
+            _entityChanges = entityChanges;
+        }
+        
+        public void Apply(T targetEntity)
+        {
+            PropertyInfo[] properties = typeof(T).GetProperties();
+            foreach (PropertyInfo property in properties)
+            {
+                object? value = property.GetValue(_entityChanges);
+                if (value == null) continue;
+                property.SetValue(targetEntity, value);
+            }
+        }
+    }
+}
