ec414b5dccd79a5660b8ab9ffb6b5e78adc1a715
AnOwlbear <AnOwlbear@protonmail.com>
2021-07-17 02:02:59 +0100

    Updated EntityUpdate to have public EntityChanges of target type

--- YoutubeDLView.Core/Common/EntityUpdate.cs
+++ YoutubeDLView.Core/Common/EntityUpdate.cs
@@ -4,32 +4,39 @@ using System.Reflection;
 
 namespace YoutubeDLView.Core.Common
 {
-    public class EntityUpdate<T>
+    public class EntityUpdate<T> where T : new()
     {
-        private readonly object _entityChanges;
+        public readonly T EntityChanges;
+        
+        private readonly Type _entityType;
 
         public EntityUpdate(object entityChanges)
         {
-            _entityChanges = entityChanges;
+            // Initialises changes and type objects
+            EntityChanges = new T();
+            _entityType = typeof(T);
+            
+            PropertyInfo[] changesPropertyInfos = entityChanges.GetType().GetProperties();
+            foreach (PropertyInfo propertyInfo in changesPropertyInfos)
+            {
+                // Checks property exists on target type, then sets it
+                PropertyInfo? entityPropertyInfo = _entityType.GetProperty(propertyInfo.Name);
+                if (entityPropertyInfo == null) throw new Exception("Invalid property on entityChanges");
+                object? value = propertyInfo.GetValue(entityChanges);
+                entityPropertyInfo.SetValue(EntityChanges, value);
+            }
         }
         
         // Applies the changes to the given target entity
         public void Apply(T targetEntity, bool updateWithNull = false)
         {
-            // Gets the list of properties to update
-            PropertyInfo[] propertyInfos = _entityChanges.GetType().GetProperties();
-            Type targetType = typeof(T);
+            PropertyInfo[] propertyInfos = _entityType.GetProperties();
             foreach (PropertyInfo propertyInfo in propertyInfos)
             {
-                // Gets the property of the target entity, change checks it exists
-                PropertyInfo? targetPropertyInfo = targetType.GetProperty(propertyInfo.Name);
-                if (targetPropertyInfo == null)
-                    throw new InvalidOperationException("Target entity does not have matching properties");
-                
-                // Updates the value
-                object? value = propertyInfo.GetValue(_entityChanges);
-                if (!updateWithNull && value == null) continue;
-                targetPropertyInfo.SetValue(targetEntity, value);
+                // Checks if the value is null, and sets it if suitable
+                object? value = propertyInfo.GetValue(EntityChanges);
+                if (value == null && !updateWithNull) continue;
+                propertyInfo.SetValue(targetEntity, value);
             }
         }
     }
