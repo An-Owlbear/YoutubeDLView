490c0ed4c149d719fbd362720310744e5bd5757f
AnOwlbear <gdwgna@protonmail.com>
2021-07-23 23:46:36 +0100

    Added upload date to videos

---
+++ YoutubeDLView.Data/Migrations/20210723224236_Added upload date.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace YoutubeDLView.Data.Migrations
+{
+    public partial class Addeduploaddate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UploadDate",
+                table: "Videos",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UploadDate",
+                table: "Videos");
+        }
+    }
+}
