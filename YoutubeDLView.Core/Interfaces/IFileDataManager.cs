00ccf7fa5e0879a97c667094dc271bf9ecac01ce
AnOwlbear <anowlbear@protonmail.com>
2021-07-25 14:56:36 +0100

    Added FileDataManager and moved thumbnail retrieval method to it

---
+++ YoutubeDLView.Core/Interfaces/IFileDataManager.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using YoutubeDLView.Core.Common;
+
+namespace YoutubeDLView.Core.Interfaces
+{
+    public interface IFileDataManager
+    {
+        /// <summary>
+        /// Returns the thumbnail and mime type for the given video
+        /// </summary>
+        /// <param name="videoId">The Id of the video to retrieve the thumbnail of</param>
+        /// <returns>The thumbnail <see cref="Stream" /> and mime type</returns>
+        Task<Result<(Stream, string)>> GetThumbnail(string videoId);
+    }
+}
