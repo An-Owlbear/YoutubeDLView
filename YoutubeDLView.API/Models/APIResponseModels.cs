4c95812b91a0098e0fe30d4a4fbd243e8c00ea9f
AnOwlbear <anowlbear@protonmail.com>
2021-07-30 23:52:30 +0100

    Added response objects for database objects

--- YoutubeDLView.API/Models/APIResponseModels.cs
+++ YoutubeDLView.API/Models/APIResponseModels.cs
@@ -1,6 +1,20 @@
+using YoutubeDLView.Core.Entities;
+
 namespace YoutubeDLView.API.Models
 {
     public record LoginInformation(string UserId, string Username, string AccessToken, string RefreshToken);
 
     public record RefreshInformation(string UserId, string Username, string AccessToken);
+
+    public record ChannelResponse(string Id, string Name)
+    {
+        public ChannelResponse(YtChannel channel) : this(channel.Id, channel.Name) { }
+    }
+
+    public record VideoResponse(string Id, string Title, string Description, string UploadDate, int Length,
+        ChannelResponse ChannelResponse)
+    {
+        public VideoResponse(Video video) : this(video.Id, video.Title, video.Description, video.UploadDate,
+            video.Length, new ChannelResponse(video.Channel)) { }
+    }
 }
