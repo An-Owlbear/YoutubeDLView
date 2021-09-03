cd474190a9424f12fb02fdf90038d64df9fa5b6d
AnOwlbear <anowlbear@protonmail.com>
2021-09-03 20:06:49 +0100

    Updated main page to have a load more videos button

--- YoutubeDLView.Web/src/pages/MainPage.tsx
+++ YoutubeDLView.Web/src/pages/MainPage.tsx
@@ -1,18 +1,19 @@
-import { CircularProgress, makeStyles, useMediaQuery, useTheme } from '@material-ui/core';
+import { Button, CircularProgress, makeStyles, useMediaQuery, useTheme } from '@material-ui/core';
 import { useAtom } from 'jotai';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import VideoCard from '../components/VideoCard';
 import { VideoInformation } from '../models/apiModels';
 import { sessionAtom } from '../services/globalStore';
 import { useApiRequest } from '../services/useApiRequest';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     flexWrap: 'wrap',
-    justifyContent: 'center'
-  }
+    justifyContent: 'center',
+    marginBottom: theme.spacing(2)
+  },
 }));
 
 const MainPage: React.FC = () => {
@@ -22,35 +23,46 @@ const MainPage: React.FC = () => {
 
   const [session,] = useAtom(sessionAtom);
   const [videos, setVideos] = useState<VideoInformation[]>([]);
-  const [error, loading, sendRequest] = useApiRequest<VideoInformation[]>('/api/videos', 'get', true);
+  const [skip, setSkip] = useState(0);
+  const [maxLoaded, setMaxLoaded] = useState(false);
+  const queryParams = useMemo(() => ({skip, take: 30}), [skip]);
+  const [error, loading, sendRequest] = useApiRequest<VideoInformation[]>('/api/videos', 'get', true, { params: queryParams } );
 
   useEffect(() => {
     const loadVideos = async () => {
       const response = await sendRequest();
       if (!response) return;
+      if (response.length < 30) setMaxLoaded(true);
       setVideos([...videos, ...response]);
     };
     loadVideos();
-  }, []);
+  }, [skip]);
+
+  const handleLoadButton = () => {
+    setSkip(prevState => prevState + 30);
+  };
 
   // Redirects user to login if not logged in, otherwise returns main page content
   if (!session) return <Redirect to="/login" />;
   return (
-    <div className={classes.root}>
+    <>
+      <div className={classes.root}>
+        {
+          videos?.map(x =>
+            <VideoCard
+              key={x.id}
+              width={isSmallScreen ? '100%' : 300}
+              id={x.id}
+              title={x.title}
+              channel={x.channelResponse.name}
+              channelId={x.channelResponse.id}
+            />
+          )
+        }
+      </div>
       {loading && <CircularProgress />}
-      {!loading &&
-        videos?.map(x =>
-          <VideoCard
-            key={x.id}
-            width={isSmallScreen ? '100%' : 300}
-            id={x.id}
-            title={x.title}
-            channel={x.channelResponse.name}
-            channelId={x.channelResponse.id}
-          />
-        )
-      }
-    </div>
+      {!loading && !maxLoaded && <Button variant="contained" color="primary" onClick={handleLoadButton}>Load more</Button>}
+    </>
   );
 };
 
