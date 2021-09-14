7fa9aa2d430c31eb6c2c42125178908932228a33
AnOwlbear <anowlbear@protonmail.com>
2021-09-14 23:39:09 +0100

    Added Result class to client app

---
+++ YoutubeDLView.Web/src/models/Result.ts
@@ -0,0 +1,18 @@
+// Represents the result of an operation, indicating whether or not it was successful
+class Result<T> {
+  constructor(errorValue: string, dataValue?: T) {
+    this.data = dataValue;
+    this.error = errorValue;
+    this.success = !!this.error;
+  }
+
+  data?: T;
+  error: string;
+  success: boolean;
+
+  public static ok = <T>(data?: T): Result<T> => new Result('', data);
+
+  public static failure = <T>(error: string): Result<T> => new Result(error);
+}
+
+export default Result;
