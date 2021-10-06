83d8040b55dfd94f1462878417a09e974cf2d48c
AnOwlbear <anowlbear@protonmail.com>
2021-10-06 17:13:35 +0100

    Made preparations for migration to material-ui v5: - updated packages - installed new @mui packages - updated index.tsx to not use useStyles

---
+++ YoutubeDLView.Web/src/App.tsx
@@ -0,0 +1,60 @@
+import { CssBaseline, makeStyles } from '@material-ui/core';
+import clsx from 'clsx';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import '@fontsource/roboto';
+import AppDrawer from './components/AppDrawer';
+import TopAppBar from "./components/TopAppBar";
+import AccountPage from './pages/AccountPage';
+import ChannelListPage from './pages/ChannelListPage';
+import ChannelPage from './pages/ChannelPage';
+import LoginPage from './pages/LoginPage';
+import MainPage from './pages/MainPage';
+import SearchPage from './pages/SearchPage';
+import SetupPage from './pages/SetupPage';
+import SignupPage from './pages/SignupPage';
+import VideoPage from './pages/VideoPage';
+
+const drawerWidth = 250;
+
+const useStyles = makeStyles((theme) => ({
+  contentHeader: theme.mixins.toolbar,
+  content: {
+    margin: theme.spacing(5),
+    display: 'flex',
+    flexDirection: 'column',
+    alignContent: 'center',
+    '& > *': {
+      alignSelf: 'center'
+    },
+    [theme.breakpoints.down('xs')]: {
+      margin: theme.spacing(2)
+    }
+  }
+}));
+
+export const App: React.FC = () =>  {
+  const classes = useStyles();
+
+  return (
+    <BrowserRouter>
+      <CssBaseline />
+      <TopAppBar />
+      <AppDrawer width={drawerWidth} />
+      <div className={classes.contentHeader} />
+      <div className={clsx(classes.content)}>
+        <Switch>
+          <Route exact path="/" component={MainPage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/videos/:id" component={VideoPage} />
+          <Route exact path="/channels" component={ChannelListPage} />
+          <Route exact path="/channels/:id" component={ChannelPage} />
+          <Route exact path="/search/:search" component={SearchPage}  />
+          <Route exact path="/account" component={AccountPage} />
+          <Route exact path="/signup" component={SignupPage} />
+          <Route exact path="/setup" component={SetupPage} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+};
--- YoutubeDLView.Web/src/index.tsx
+++ YoutubeDLView.Web/src/index.tsx
@@ -1,65 +1,11 @@
-import { CssBaseline, makeStyles } from '@material-ui/core';
-import clsx from 'clsx';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import '@fontsource/roboto';
-import AppDrawer from './components/AppDrawer';
-import TopAppBar from "./components/TopAppBar";
-import AccountPage from './pages/AccountPage';
-import ChannelListPage from './pages/ChannelListPage';
-import ChannelPage from './pages/ChannelPage';
-import LoginPage from './pages/LoginPage';
-import MainPage from './pages/MainPage';
-import SearchPage from './pages/SearchPage';
-import SetupPage from './pages/SetupPage';
-import SignupPage from './pages/SignupPage';
-import VideoPage from './pages/VideoPage';
+import { App } from './App';
 
-const drawerWidth = 250;
+const theme = createMuiTheme();
 
-const useStyles = makeStyles((theme) => ({
-  contentHeader: theme.mixins.toolbar,
-  content: {
-    margin: theme.spacing(5),
-    display: 'flex',
-    flexDirection: 'column',
-    alignContent: 'center',
-    '& > *': {
-      alignSelf: 'center'
-    },
-    [theme.breakpoints.down('xs')]: {
-      margin: theme.spacing(2)
-    }
-  }
-}));
-
-const App = () =>  {
-  const classes = useStyles();
-
-  return (
-    <div>
-      <BrowserRouter>
-        <CssBaseline />
-        <TopAppBar />
-        <AppDrawer width={drawerWidth} />
-        <div className={classes.contentHeader} />
-        <div className={clsx(classes.content)}>
-          <Switch>
-            <Route exact path="/" component={MainPage} />
-            <Route exact path="/login" component={LoginPage} />
-            <Route exact path="/videos/:id" component={VideoPage} />
-            <Route exact path="/channels" component={ChannelListPage} />
-            <Route exact path="/channels/:id" component={ChannelPage} />
-            <Route exact path="/search/:search" component={SearchPage}  />
-            <Route exact path="/account" component={AccountPage} />
-            <Route exact path="/signup" component={SignupPage} />
-            <Route exact path="/setup" component={SetupPage} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    </div>
-  );
-};
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ThemeProvider theme={theme}><App /></ThemeProvider>,
+  document.getElementById('root')
+);
